5a3eabe7b2ad05f481491564048e9af7382a08a4
aingelsten <aingelsten@gmail.com>
2022-10-04 19:47:53 +0100

    kotlin lab week 4

--- src/main/kotlin/org/setu/placemark/console/main/main.kt
+++ src/main/kotlin/org/setu/placemark/console/main/main.kt
@@ -13,72 +13,7 @@ val placemarkView = PlacemarkView()
 val controller = PlacemarkController()
 
 fun main(args: Array<String>) {
-    logger.info { "Launching Placemark Console App" }
-    println("Placemark Kotlin App Version 1.0")
-
-    var input: Int
-
-    do {
-        input = placemarkView.menu()
-        when(input) {
-            1 -> addPlacemark()
-            2 -> updatePlacemark()
-            3 -> placemarkView.listPlacemarks(placemarks)
-            4 -> searchPlacemark()
-            -99 -> dummyData()
-            -1 -> println("Exiting App")
-            else -> println("Invalid Option")
-        }
-        println()
-    } while (input != -1)
-    logger.info { "Shutting Down Placemark Console App" }
+    PlacemarkController().start()
 }
-
-fun addPlacemark(){
-    val aPlacemark = PlacemarkModel()
-
-    if (placemarkView.addPlacemarkData(aPlacemark))
-        placemarks.create(aPlacemark)
-    else
-        logger.info("Placemark Not Added")
-}
-
-fun updatePlacemark() {
-
-    placemarkView.listPlacemarks(placemarks)
-    var searchId = placemarkView.getId()
-    val aPlacemark = search(searchId)
-
-    if(aPlacemark != null) {
-        if(placemarkView.updatePlacemarkData(aPlacemark)) {
-            placemarks.update(aPlacemark)
-            placemarkView.showPlacemark(aPlacemark)
-            logger.info("Placemark Updated : [ $aPlacemark ]")
-        }
-        else
-            logger.info("Placemark Not Updated")
-    }
-    else
-        println("Placemark Not Updated...")
-}
-
-fun searchPlacemark() {
-    val aPlacemark = search(placemarkView.getId())!!
-    placemarkView.showPlacemark(aPlacemark)
-}
-
-
-fun search(id: Long) : PlacemarkModel? {
-    var foundPlacemark = placemarks.findOne(id)
-    return foundPlacemark
-}
-
-fun dummyData() {
-    placemarks.create(PlacemarkModel(title = "New York New York", description = "So Good They Named It Twice"))
-    placemarks.create(PlacemarkModel(title= "Ring of Kerry", description = "Some place in the Kingdom"))
-    placemarks.create(PlacemarkModel(title = "Waterford City", description = "You get great Blaas Here!!"))
-}
-
-
 
 
